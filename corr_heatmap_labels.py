c8789ec957c11a3b0d401d41ea07221c6bd8f0e7
Thorben <tho.maass@uni-luebeck.de>
2023-07-20 09:32:45 +0200

    added correlation heatmap and script to generate

---
+++ corr_heatmap_labels.py
@@ -0,0 +1,87 @@
+"""
+Description:        script genereates correlation heatmap and csv file from experimental labels. prepares data as it is done in scripts for training and evaluation.
+
+Inputs:             Input 1 are labels for model training and evaluation. Input 2 are sequences for  model training and evaluation. 
+
+further parameters: can be specified in the parameters section of this script. 
+
+Example commands:   python corr_heatmap_labels.py 2023-01-10_22-29-33\ myCounts.minDNAfilt.depthNorm.keepHaps\ -\ starr.haplotypes.oligo1.txt starrseq-all-final-toorder_oligocomposition.csv
+
+Outputs:            correlation heatmap and correlation csv file
+
+"""
+
+
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import numpy as np
+
+#parameters
+sequence_length=198
+
+#import labels
+df_IDs_reg_labels=pd.read_csv(sys.argv[1], sep="\t", decimal=',', low_memory=False)
+df_IDs_reg_labels=df_IDs_reg_labels.drop_duplicates()
+
+#replace 0 with 1 for log transformation later
+df_IDs_reg_labels=df_IDs_reg_labels.replace(0, 1)
+
+#import sequences
+df_IDs_Sequences=pd.read_csv(sys.argv[2], sep=",",low_memory=False)
+
+#remove ">" as first character from ID column
+df_IDs_Sequences["name"]=df_IDs_Sequences["name"].str[1:]
+
+#convert seqs to list and drop sequences with wrong sequence length
+sequence_list=df_IDs_Sequences['enhancer'].to_list()
+sequences_tok=[]
+
+for i in range (0, len(sequence_list), 1): 
+    if  len(sequence_list[i])==sequence_length:
+        continue
+    else:
+        sequences_tok.append(np.nan)
+
+#drop duplicates
+df_IDs_Sequences=df_IDs_Sequences.dropna()
+
+#merge data frames on name/oligo columns
+df_IDs_Sequences=df_IDs_Sequences.rename(columns={'name': 'ID'})
+df_IDs_reg_labels=df_IDs_reg_labels.rename(columns={'Oligo': 'ID'})
+df_IDs_seqs_reg_labels=pd.merge(df_IDs_Sequences, df_IDs_reg_labels, on=["ID"])
+
+#average data over replica and add them to data frame
+#3T3
+df_IDs_seqs_reg_labels['mean_cell_3T3_diff_CTRL'] = df_IDs_seqs_reg_labels.loc[:, ["cell_3T3_diff_CTRL_rep1_2022_12_14", "cell_3T3_diff_CTRL_rep2_2022_12_14", "cell_3T3_diff_CTRL_rep3_2022_12_14", "cell_3T3_diff_CTRL_rep4_2022_12_14"]].mean(axis=1)
+df_IDs_seqs_reg_labels['mean_ccell_3T3_undiff_CTRL'] = df_IDs_seqs_reg_labels.loc[:, ["cell_3T3_undiff_CTRL_rep1_2022_12_14", "cell_3T3_undiff_CTRL_rep2_2022_12_14", "cell_3T3_undiff_CTRL_rep3_2022_12_14", "cell_3T3_undiff_CTRL_rep4_2022_12_14"]].mean(axis=1)
+df_IDs_seqs_reg_labels['mean_cell_3T3_undiff_TGFB'] = df_IDs_seqs_reg_labels.loc[:, ["cell_3T3_undiff_TGFB_rep1_2022_12_14", "cell_3T3_undiff_TGFB_rep2_2022_12_14", "cell_3T3_undiff_TGFB_rep3_2022_12_14", "cell_3T3_undiff_TGFB_rep4_2022_12_14"]].mean(axis=1)
+#RAW
+df_IDs_seqs_reg_labels['mean_RAW_CTRL'] = df_IDs_seqs_reg_labels.loc[:, ["RAW_CTRL_rep1_2022_12_14", "RAW_CTRL_rep2_2022_12_14", "RAW_CTRL_rep3_2022_12_14", "RAW_CTRL_rep4_2022_12_14"]].mean(axis=1)
+df_IDs_seqs_reg_labels['mean_RAW_IL1B'] = df_IDs_seqs_reg_labels.loc[:, ["RAW_IL1B_rep1_2022_12_14", "RAW_IL1B_rep2_2022_12_14", "RAW_IL1B_rep3_2022_12_14", "RAW_IL1B_rep4_2022_12_14"]].mean(axis=1)
+df_IDs_seqs_reg_labels['mean_RAW_TGFB'] = df_IDs_seqs_reg_labels.loc[:, ["RAW_TGFB_rep1_2022_12_14", "RAW_TGFB_rep2_2022_12_14", "RAW_TGFB_rep3_2022_12_14", "RAW_TGFB_rep4_2022_12_14"]].mean(axis=1)
+#TeloHAEC
+df_IDs_seqs_reg_labels['mean_TeloHAEC_CTRL'] = df_IDs_seqs_reg_labels.loc[:, ["TeloHAEC_CTRL_rep1", "TeloHAEC_CTRL_rep2", "TeloHAEC_CTRL_rep3"]].mean(axis=1)
+df_IDs_seqs_reg_labels['mean_TeloHAEC_IL1b_24h'] = df_IDs_seqs_reg_labels.loc[:, ["TeloHAEC_IL1b_24h_rep1", "TeloHAEC_IL1b_24h_rep2", "TeloHAEC_IL1b_24h_rep3"]].mean(axis=1)
+df_IDs_seqs_reg_labels['mean_TeloHAEC_IL1b_6h'] = df_IDs_seqs_reg_labels.loc[:, ["TeloHAEC_IL1b_6h_rep1", "TeloHAEC_IL1b_6h_rep2", "TeloHAEC_IL1b_6h_rep3"]].mean(axis=1)
+#HASMC
+df_IDs_seqs_reg_labels['mean_HASMC_untreatedPilot'] = df_IDs_seqs_reg_labels.loc[:, ["HASMC_untreatedPilot_rep1", "HASMC_untreatedPilot_rep2", "HASMC_untreatedPilot_rep3"]].mean(axis=1)
+df_IDs_seqs_reg_labels['mean_HASMC_Chol'] = df_IDs_seqs_reg_labels.loc[:, ["HASMC_Chol_rep1", "HASMC_Chol_rep2", "HASMC_Chol_rep3"]].mean(axis=1)
+#HepG2
+df_IDs_seqs_reg_labels['mean_HepG2_untreatedPilot'] = df_IDs_seqs_reg_labels.loc[:, ["HepG2_untreatedPilot_rep1", "HepG2_untreatedPilot_rep2", "HepG2_untreatedPilot_rep3"]].mean(axis=1)
+
+#plot data (https://stackoverflow.com/questions/29432629/plot-correlation-matrix-using-pandas)
+f = plt.figure(figsize=(29, 25))
+plt.matshow(df_IDs_seqs_reg_labels.select_dtypes(['number']).corr(), fignum=f.number)
+plt.xticks(range(df_IDs_seqs_reg_labels.select_dtypes(['number']).shape[1]), df_IDs_seqs_reg_labels.select_dtypes(['number']).columns, fontsize=12, rotation=90)
+plt.yticks(range(df_IDs_seqs_reg_labels.select_dtypes(['number']).shape[1]), df_IDs_seqs_reg_labels.select_dtypes(['number']).columns, fontsize=12)
+cb = plt.colorbar()
+cb.ax.tick_params(labelsize=12)
+plt.title('Correlation Matrix', fontsize=16)
+plt.savefig("corr_heatmap_exp_labels.svg")
+plt.close()
+
+#save correlations as csv
+df_IDs_seqs_reg_labels.select_dtypes(['number']).corr().to_csv("correlations_exp_labels.csv", sep='\t')
